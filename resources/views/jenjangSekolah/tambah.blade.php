b9f22abc6d9b28c4e695fda2ff2f2b1dc6278f81
Ahmad Syauqi <ahmadsyauqi@merahputih.id>
2018-09-16 02:08:32 +0800

    perbaikan halaman tambah data jenjang

--- resources/views/jenjangSekolah/tambah.blade.php
+++ resources/views/jenjangSekolah/tambah.blade.php
@@ -9,24 +9,22 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="table-responsive">
-                <form action="" class="form-horizontal">
-                    <div class="form-group row">
-                        <label class="col-xl-2 col-form-label text-bold">Nama Jenjang</label>
-                        <div class="col-xl-10">
-                            <input class="form-control" type="text" placeholder="Nama Jenjang">
-                        </div>
+            <form action="" class="form-horizontal">
+                <div class="form-group row">
+                    <label class="col-xl-2 col-form-label text-bold">Nama Jenjang</label>
+                    <div class="col-xl-10">
+                        <input class="form-control" type="text" placeholder="Nama Jenjang">
                     </div>
-                    <div class="form-group row">
-                        <label class="col-xl-2 col-form-label text-right"></label>
-                        <div class="col-xl-10">
-                            <button type="submit" class="btn btn-labeled btn-info btn">
-                                <span class="btn-label"><i class="fa fa-save"></i></span><b>Simpan</b>
-                            </button>
-                            <button type="reset" class="btn btn-labeled btn-danger btn">
-                                <span class="btn-label"><i class="fa fa-undo"></i></span><b>Reset</b>
-                            </button>
-                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-xl-2 col-form-label text-right"></label>
+                    <div class="col-xl-10">
+                        <button type="submit" class="btn btn-labeled btn-info btn">
+                        <span class="btn-label"><i class="fa fa-save"></i></span><b>Simpan</b>
+                        </button>
+                        <button type="reset" class="btn btn-labeled btn-danger btn">
+                        <span class="btn-label"><i class="fa fa-undo"></i></span><b>Reset</b>
+                        </button>
                     </div>
                 </div>
             </form>
