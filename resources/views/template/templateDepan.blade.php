d42625ad05144b2fbe2b4ea7e41f99c0cf49f15a
Ahmad Syauqi <ahmadsyauqi@merahputih.id>
2018-09-20 21:04:00 +0800

    template halaman depan

--- resources/views/template/templateDepan.blade.php
+++ resources/views/template/templateDepan.blade.php
@@ -1,47 +1,44 @@
-<html lang="en">
-<head>
-    <title>Page Not Found</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <!-- style pake yg di 404 css -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-    <body class="antialiased font-sans">
-        <div class="md:flex min-h-screen">
-            <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
-                <div class="max-w-sm m-8">
-                    <div class="text-black text-5xl md:text-15xl font-black">
-                        404 </div>
-                    <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
-                    <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
-                        Sorry, the page you are looking for could not be found. </p>
-                    <a href="/">
-                        <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-                            Go Home
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <div class="pb-full flex md:pb-0 items-center md:min-h-screen w-full md:w-1/2 items-center justify-center">
-                <div class="max-w-sm m-8">
+@extends('layouts/masterDepan')
+@section('content-heading','Login Tambah')
+@section('content')
+<body class="layout-h" style="margin-top: 10%">
+    <div class="wrapper">
+            <section class="section-container">
+                <div class="content-wrapper">
                     <div class="row">
-                        <div class="col-md-12">
-
-                            <div class="card card-default">
-                                <div class="card-header">
-                                <div class="card-title">Login</div>
-                            </div>
-                            <div class="card card-body">
-                                <form action="">
-                                    <div class="form-group"><label for=""></label><input type="text" class="form-control"></div>
-                                </form>
-                            </div>
+                        <div class="col-xl-8"></div>
+                        <div class="col-xl-4">
+                            <div class="row mr-3">
+                                <div class="col-xl-12">
+                                    <div class="card card-default">
+                                        <div class="card-header">Login</div>
+                                        <div class="card-body">
+                                            <form action="">
+                                                <div class="form-grop">
+                                                    <label for="">Username</label>
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                                <div class="form-grop">
+                                                    <label for="">Password</label>
+                                                    <input type="password" class="form-control">
+                                                </div>
+                                                <br>
+                                                <div class="clear-fix">
+                                                    <div class="float-right">
+                                                        <a href="">Lupa Password</a>
+                                                    </div>
+                                                </div>
+                                                <br>
+                                                <div class="form-group"><button class="btn btn-md btn-info">Login</button></div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </body>
-</html>
+            </section>
+        </div>    
+</body>
+@endsection
