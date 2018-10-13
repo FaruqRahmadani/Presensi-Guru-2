1c66861013aea93d78ed9754ceaff7baf2948b8c
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-14 01:14:16 +0800

    seeder initial user admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,10 +14,13 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-    ];
+  return [
+    'nama' => $faker->name,
+    'username' => $faker->unique()->username,
+    'password' => 'rahasia',
+    'email' => $faker->unique()->safeEmail,
+    'tipe' => $faker->numberBetween(1,2),
+    // 'sekolah_id' => $faker->numberBetween(1,2),
+    'remember_token' => str_random(10),
+  ];
 });
