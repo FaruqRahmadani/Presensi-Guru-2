187309808c15a821fb9b0f693f9fe017c193ab56
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-14 09:30:44 +0800

    login route

--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,6 @@ Route::Group(['middleware' => ['AuthMiddleware']], function(){
   });
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
