dccd5f742f0da758471b33992092c7ef40d7e0ff
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-14 22:37:24 +0800

    pindah js datatable ke global

--- resources/views/jenjangSekolah/data.blade.php
+++ resources/views/jenjangSekolah/data.blade.php
@@ -10,7 +10,7 @@
       </div>
       <div class="card-body">
         <div class="table-responsive">
-          <table width="100%" class="table table-striped table-bordered tabel-custom my-4 w-100" id="datatable1">
+          <table width="100%" class="table table-striped table-bordered tabel-custom my-4 w-100" id="datatable">
             <thead>
               <tr>
                 <th>#</th>
@@ -37,28 +37,4 @@
       </div>
     </div>
   </div>
-@endsection
-@section('js-content')
-  <script>
-  $('#datatable1').DataTable({
-    'paging': true,
-    'ordering': true,
-    'info': true,
-    responsive: true,
-    oLanguage: {
-      sSearch: '<em class="fas fa-search"></em>',
-      sEmptyTable:   "Tidak ada data yang tersedia pada tabel ini",
-      sProcessing:   "Sedang memproses...",
-      sLengthMenu:   "Tampilkan _MENU_ entri",
-      sZeroRecords:  "Tidak ditemukan data yang sesuai",
-      sInfo:         "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
-      sInfoEmpty:    "Menampilkan 0 sampai 0 dari 0 entri",
-      sInfoFiltered: "(disaring dari _MAX_ entri keseluruhan)",
-      oPaginate: {
-        sNext: '<em class="fa fa-caret-right"></em>',
-        sPrevious: '<em class="fa fa-caret-left"></em>'
-      }
-    }
-  });
-</script>
 @endsection
