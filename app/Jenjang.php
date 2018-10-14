4db44812bc992d5e26abb0da9a8af9d827152990
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-14 23:58:38 +0800

    accessor uuid jenjang

--- app/Jenjang.php
+++ app/Jenjang.php
@@ -3,11 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use HCrypt;
 
 class Jenjang extends Model
 {
   protected $fillable = ['nama'];
+
   public function Sekolah(){
     return $this->hasMany('App\Sekolah');
+  }
+
+  public function getUUIDAttribute(){
+    return HCrypt::Encrypt($this->id);
   }
 }
