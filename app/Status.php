1780e21ec349bab61cac00e64b93b68e46e5b35a
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-16 01:51:40 +0800

    menambahkan accessor uuid dan relasi dengan sekolah

--- app/Status.php
+++ app/Status.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+  public function Sekolah(){
+    return $this->hasMany('App\Sekolah');
+  }
+
+  public function getUUIDAttribute(){
+    return encrypt($this->id);
+  }
 }
