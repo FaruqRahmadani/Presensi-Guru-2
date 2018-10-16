2be7432b7501a26a58fb0ee82808526b0888866e
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-17 00:23:29 +0800

    fungsi soft delete data kategori absen

--- app/KategoriAbsen.php
+++ app/KategoriAbsen.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KategoriAbsen extends Model
 {
+  use SoftDeletes;
+
   protected $fillable = ['kode', 'keterangan', 'kode_warna'];
 
   public function getUUIDAttribute(){
