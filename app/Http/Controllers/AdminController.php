a730c1968734fa7578db9e189a596b22d21f0292
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-17 01:03:22 +0800

    tampilan data admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
 
 class AdminController extends Controller
 {
-  public function data(){
-    return view('dataAdmin.data');
+  public function data(UserRepository $user){
+    $user = $user->where('tipe', 1);
+    return view('admin.data', compact('user'));
   }
 }
---
+++ resources/views/admin/data.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+@section('content-heading','Data Admin')
+@section('content')
+  <div class="container-fluid">
+    <div class="card card-default">
+      <div class="card-header">
+        <div class="card-title">
+          <a href="/data-admin/tambah" class="btn btn-labeled btn-info btn-md"><span class="btn-label"><i class="fa fa-plus"></i></span><b>Tambah Data</b></a>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table width="100%" class="table table-striped table-bordered tabel-custom my-4 w-100" id="datatable">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nama</th>
+                <th>E-Mail</th>
+                <th>Username</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($user as $dataUser)
+                <tr>
+                  <td>{{$loop->iteration}}</td>
+                  <td>
+                    <div class="inline">
+                      <img class="rounded-circle thumb48" src="{{ asset($dataUser->foto)}}" alt="">
+                    </div>
+                    <div class="inline">
+                      <strong>{{$dataUser->nama}}</strong>
+                    </div>
+                  </td>
+                  <td>{{$dataUser->email}}</td>
+                  <td>{{$dataUser->username}}</td>
+                  <td>
+                    <a href="#" class="btn btn-labeled btn-primary btn-xs"><span class="btn-label"><i class="fa fa-edit"></i></span>Edit</a>
+                    <a href="#" class="btn btn-labeled btn-danger btn-xs"><span class="btn-label"><i class="fa fa-trash"></i></span>Hapus</a>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
