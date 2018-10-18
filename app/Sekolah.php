5d86dde01422ac384b654fd29eacfbdcc0f8a2d2
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-19 01:34:50 +0800

    menambahkan relasi tabel sekolah

--- app/Sekolah.php
+++ app/Sekolah.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sekolah extends Model
 {
-    //
+  public function Pegawai(){
+    return $this->belongsTo('App\Pegawai');
+  }
+
+  public function Jenjang(){
+    return $this->belongsTo('App\Jenjang');
+  }
+
+  public function Status(){
+    return $this->belongsTo('App\Status');
+  }
+
+  public function Kecamatan(){
+    return $this->belongsTo('App\Kecamatan');
+  }
+
+  public function Kelurahan(){
+    return $this->belongsTo('App\Kelurahan');
+  }
+
+  public function getNamaKepsekAttribute(){
+    if (!$this->pegawai_id) {
+      return "Belum Diisi";
+    }
+    return $this->Pegawai->nama;
+  }
 }
