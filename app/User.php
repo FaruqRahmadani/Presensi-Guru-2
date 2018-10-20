b38f7da2954f439940b4c168f7b11de7449e9770
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-20 20:17:55 +0800

    menambahkan sekolah_id jadi fillable

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
   * @var array
   */
   protected $fillable = [
-    'nama', 'email', 'username', 'password', 'foto', 'tipe'
+    'nama', 'email', 'username', 'password', 'foto', 'tipe', 'sekolah_id'
   ];
 
   /**
