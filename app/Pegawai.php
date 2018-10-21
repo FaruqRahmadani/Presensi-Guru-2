d1eb27d97b1eea24b791db379c5bdf834133c4f6
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-21 10:17:29 +0800

    menambahkan softdelete di data pegawai

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use HDate;
 
 class Pegawai extends Model
 {
+  use SoftDeletes;
+
   protected $fillable = ['nip', 'nama', 'nuptk', 'sekolah_id', 'tanggal_lahir', 'tempat_lahir', 'jenis_kelamin', 'no_handphone', 'email', 'alamat', 'foto', 'sidikjari_id'];
 
   public function getTTLAttribute(){
