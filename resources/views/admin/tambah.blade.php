fc85211165918a96b885103cef56994265fefa48
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-10-21 18:30:39 +0800

    enhancement edit and add admin

--- resources/views/admin/tambah.blade.php
+++ resources/views/admin/tambah.blade.php
@@ -26,7 +26,8 @@
           <div class="form-group row">
             <label class="col-xl-2 col-form-label text-bold">Foto</label>
             <div class="col-lg-10">
-              <input class="form-control" type="file" name="foto" accept="image/*">
+              {{-- <input class="form-control" type="file" name="foto" accept="image/*"> --}}
+               <input class="form-control filestyle" type="file" name="foto" accept="image/*" data-classbutton="btn btn-secondary" data-classinput="form-control inline" data-text="Pilih Foto" data-buttonBefore="true" data-icon="&lt;span class='fa fa-upload mr-2'&gt;&lt;/span&gt;">
               <span>Ukuran Foto 1:1 </span>
             </div>
           </div>
