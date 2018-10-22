b8cc24e0e18dacd674cebe82ba8b24f59335a259
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-22 22:51:13 +0800

    fungsi edit profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Validation\Rule;
 use Validator;
 use Auth;
 use Hash;
+use File;
 
 class ProfileController extends Controller
 {
@@ -16,6 +17,7 @@ class ProfileController extends Controller
   }
 
   public function editSubmit(UserRepository $user, Request $request){
+    $data = [];
     Validator::make(
       $request->all(),[
         'username' => Rule::unique('users')->ignore(Auth::user()->id),
@@ -29,5 +31,17 @@ class ProfileController extends Controller
     )->validate();
     $passwordOldConf = $request->password_old? (Hash::check($request->password_old, Auth::User()->password)? 1:0):1;
     if (!$passwordOldConf) return redirect()->back()->withInput()->with(['alert' => true, 'tipe' => 'error', 'judul' => 'Error', 'pesan' => 'Password Lama Salah']);
+    if ($request->foto) {
+      if (!str_is('*default.png', Auth::User()->foto)) {
+        File::delete(Auth::User()->foto);
+      }
+      $FotoExt = $request->foto->getClientOriginalExtension();
+      $FotoName = "[super_admin]$request->nama.$request->_token";
+      $Foto = "{$FotoName}.{$FotoExt}";
+      $path = $request->foto->move('img/user', $Foto);
+      $data = ['foto' => $path];
+    }
+    $user->update(Auth::User()->id, array_merge($request->all(), $data));
+    return redirect()->route('profileEditForm')->with(['alert' => true, 'tipe' => 'success', 'judul' => 'Berhasil', 'pesan' => 'Profile Anda Telah Diubah']);
   }
 }
