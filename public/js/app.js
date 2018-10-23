2763532f4076551c157f7ea142db80afdf01b3fc
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-10-23 23:13:08 +0800

    rendered

--- public/js/app.js
+++ public/js/app.js
@@ -29216,6 +29216,7 @@ __webpack_require__(55);
 __webpack_require__(56);
 __webpack_require__(57);
 __webpack_require__(85);
+__webpack_require__(86);
 
 /***/ }),
 /* 15 */
@@ -76190,6 +76191,43 @@ $(document).ready(function () {
         readURL(this);
     });
 });
+
+/***/ }),
+/* 86 */
+/***/ (function(module, exports) {
+
+// Morris
+// -----------------------------------
+(function (window, document, $, undefined) {
+  'use strict';
+
+  $(initMorris);
+  function initMorris() {
+    if (typeof Morris === 'undefined') return;
+    var chartdata = [{ y: "2006", a: 100, b: 90 }, { y: "2007", a: 75, b: 65 }, { y: "2008", a: 50, b: 40 }, { y: "2009", a: 75, b: 65 }, { y: "2010", a: 50, b: 40 }, { y: "2011", a: 75, b: 65 }, { y: "2012", a: 100, b: 90 }];
+    var donutdata = [{ label: "Download Sales", value: 12 }, { label: "In-Store Sales", value: 30 }, { label: "Mail-Order Sales", value: 20 }];
+    // Donut Chart
+    // -----------------------------------
+    new Morris.Donut({
+      element: 'morris-donut',
+      data: donutdata,
+      colors: ['#f05050', '#fad732', '#ff902b'],
+      resize: true
+    });
+    // Bar Chart
+    // -----------------------------------
+    new Morris.Bar({
+      element: 'morris-bar',
+      data: chartdata,
+      xkey: 'y',
+      ykeys: ["a", "b"],
+      labels: ["Series A", "Series B"],
+      xLabelMargin: 2,
+      barColors: ['#23b7e5', '#f05050'],
+      resize: true
+    });
+  }
+})(window, document, window.jQuery);
 
 /***/ })
 /******/ ]);
