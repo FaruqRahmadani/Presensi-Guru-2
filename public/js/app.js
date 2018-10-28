e62fad0de26b11b60d83051ffb3d0c11d82af0ad
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-10-28 23:48:46 +0800

    js gasan si moris

--- public/js/app.js
+++ public/js/app.js
@@ -76172,26 +76172,33 @@ $(document).ready(function () {
   $(initMorris);
   function initMorris() {
     if (typeof Morris === 'undefined') return;
-    var chartdata = [{ y: "2006", a: 100, b: 90 }, { y: "2007", a: 75, b: 65 }, { y: "2008", a: 50, b: 40 }, { y: "2009", a: 75, b: 65 }, { y: "2010", a: 50, b: 40 }, { y: "2011", a: 75, b: 65 }, { y: "2012", a: 100, b: 90 }];
-    var donutdata = [{ label: "Sekolah Dasar", value: 12 }, { label: "Sekolah Menegah Pertama", value: 30 }, { label: "Sekolah Menegah Atas", value: 20 }];
+
+    var statistikPegawai = [{ jenjang: "jenjang 1", jumlahPegawai: 100 }, { jenjang: "jenjang 2", jumlahPegawai: 75 }, { jenjang: "jenjang 3", jumlahPegawai: 50 }, { jenjang: "jenjang 4", jumlahPegawai: 75 }, { jenjang: "jenjang 5", jumlahPegawai: 50 }, { jenjang: "jenjang 6", jumlahPegawai: 75 }, { jenjang: "jenjang 7", jumlahPegawai: 100 }];
+
+    var statistikJenjang = [{ label: "Jenjang 1", value: 12 }, { label: "Jenjang 2", value: 13 }, { label: "Jenjang 3", value: 14 }, { label: "Jenjang 4", value: 15 }, { label: "Jenjang 5", value: 16 }, { label: "Jenjang 6", value: 17 }, { label: "Jenjang 7", value: 18 }, { label: "Jenjang 8", value: 19 }, { label: "Jenjang 9", value: 20 }];
+
     // Donut Chart
     // -----------------------------------
     new Morris.Donut({
+      data: statistikJenjang,
+      xkey: 'value',
+      ykeys: ["label"],
       element: 'morris-donut',
-      data: donutdata,
-      colors: ['#F05051', '#5D9BEC', '#DDE6E9'],
       resize: true
     });
+
     // Bar Chart
     // -----------------------------------
     new Morris.Bar({
       element: 'morris-bar',
-      data: chartdata,
-      xkey: 'y',
-      ykeys: ["a", "b"],
-      labels: ["Kategori Absen 1", "Kategori Absen 2"],
+      data: statistikPegawai,
+      xkey: 'jenjang',
+      ykeys: ["jumlahPegawai"],
+      labels: ["Jumlah Pegawai"],
       xLabelMargin: 2,
-      barColors: ['#23b7e5', '#f05050'],
+      barColors: ['#23b7e5'],
+      xLabelAngle: 45,
+      hideHover: 'auto',
       resize: true
     });
   }
