85987b760bb599ec194024a73f78f6fb18e07580
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-11-11 02:24:48 +0800

    menampilkan data sekolah dan routing input di data presensi sekolah

--- resources/views/presensiSekolah/data.blade.php
+++ resources/views/presensiSekolah/data.blade.php
@@ -1,48 +1,48 @@
 @extends('layouts.master')
 @section('content-heading')
-<span>Data Presensi</span>
-<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, iste.</small>{{-- Jenjang - Nama Sekolah --}}
+  <span>Data Presensi</span>
+  <small>{{Auth::User()->Sekolah->Jenjang->nama}} - {{Auth::User()->Sekolah->nama}}</small>
 @endsection
 @section('content')
-<div class="container-fluid">
-  <div class="card card-default">
-    <div class="card-header">
-      <div class="card-title">
-        <a href="/input-presensi-sekolah" class="btn btn-labeled btn-info btn-md"><span class="btn-label"><i class="fa fa-plus"></i></span><b>Tambah Data</b></a>
+  <div class="container-fluid">
+    <div class="card card-default">
+      <div class="card-header">
+        <div class="card-title">
+          <a href="{!!route('presensiSekolahInputForm')!!}" class="btn btn-labeled btn-info btn-md"><span class="btn-label"><i class="fa fa-plus"></i></span><b>Tambah Data</b></a>
+        </div>
       </div>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-striped table-bordered tabel-custom my-4 w-100" id="datatable">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Tanggal</th>
-              @foreach ($kategoriAbsen as $dataKategoriAbsen)
-                <th><i class="fa fa-circle" style="color:{{$dataKategoriAbsen->kode_warna}}"></i> {{$dataKategoriAbsen->kode}}</th>
-              @endforeach
-              <th><i class="fa fa-circle text-dark"></i> Tanpa Keterangan</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($absensi as $tanggal=>$dataAbsensi)
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered tabel-custom my-4 w-100" id="datatable">
+            <thead>
               <tr>
-                <td>{{$loop->iteration}}</td>
-                <td>{{$tanggal}}</td>
+                <th>#</th>
+                <th>Tanggal</th>
                 @foreach ($kategoriAbsen as $dataKategoriAbsen)
-                  <td class="text-center">{{$dataAbsensi->where('kategori_absen_id', $dataKategoriAbsen->id)->count()}}</td>
+                  <th><i class="fa fa-circle" style="color:{{$dataKategoriAbsen->kode_warna}}"></i> {{$dataKategoriAbsen->kode}}</th>
                 @endforeach
-                <td class="text-center">{{$pegawai->count() - $dataAbsensi->count()}}</td>
-                <td>
-                  <a href="/info-presensi-sekolah" class="btn btn-labeled btn-primary btn-xs"><i class="fa fa-info-circle"></i> Info</a>
-                </td>
+                <th><i class="fa fa-circle text-dark"></i> Tanpa Keterangan</th>
+                <th>Aksi</th>
               </tr>
-            @endforeach
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              @foreach ($absensi as $tanggal=>$dataAbsensi)
+                <tr>
+                  <td>{{$loop->iteration}}</td>
+                  <td>{{$tanggal}}</td>
+                  @foreach ($kategoriAbsen as $dataKategoriAbsen)
+                    <td class="text-center">{{$dataAbsensi->where('kategori_absen_id', $dataKategoriAbsen->id)->count()}}</td>
+                  @endforeach
+                  <td class="text-center">{{$pegawai->count() - $dataAbsensi->count()}}</td>
+                  <td>
+                    <a href="/info-presensi-sekolah" class="btn btn-labeled btn-primary btn-xs"><i class="fa fa-info-circle"></i> Info</a>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-</div>
 @endsection
