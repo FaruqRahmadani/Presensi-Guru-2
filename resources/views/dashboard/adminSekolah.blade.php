848d14d55e596f8100c4aee2f3328d29bf8da775
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-25 16:42:36 +0800

    layouting halaman dashboard

--- resources/views/dashboard/adminSekolah.blade.php
+++ resources/views/dashboard/adminSekolah.blade.php
@@ -2,24 +2,22 @@
 @section('content-heading','Dashboard')
 @section('content')
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-lg-7">
-			<div class="card card-default">
+	<div class="mb-4">
+		<div class="card-deck">
+			<div class="card card-default col-md-8">
 				<div class="card-header">Statistik Absensi Pegawai</div>
 				<div class="card-body">
 					<div>
 						<canvas id="chart-absensi-pegawai"></canvas>
-	        </div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-lg-5">
-			<div class="card card-default">
+			<div class="card card-default col-md-4">
 				<div class="card-header">Statistik Kategori Abesnsi</div>
 				<div class="card-body">
 					<div>
-          	<canvas id="chart-kategori-absensi"></canvas>
-          </div>
+						<canvas id="chart-kategori-absensi"></canvas>
+					</div>
 				</div>
 			</div>
 		</div>
