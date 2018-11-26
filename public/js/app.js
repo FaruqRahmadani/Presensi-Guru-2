e35b09c09b8db911a317bf4a768b7083937393f5
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-26 15:37:42 +0800

    public resource

--- public/js/app.js
+++ public/js/app.js
@@ -88245,6 +88245,7 @@ __webpack_require__(62);
 __webpack_require__(63);
 __webpack_require__(64);
 __webpack_require__(65);
+__webpack_require__(93);
 
 /***/ }),
 /* 62 */
@@ -88457,6 +88458,84 @@ $("#statistikPresensi").ready(function () {
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 67 */,
+/* 68 */,
+/* 69 */,
+/* 70 */,
+/* 71 */,
+/* 72 */,
+/* 73 */,
+/* 74 */,
+/* 75 */,
+/* 76 */,
+/* 77 */,
+/* 78 */,
+/* 79 */,
+/* 80 */,
+/* 81 */,
+/* 82 */,
+/* 83 */,
+/* 84 */,
+/* 85 */,
+/* 86 */,
+/* 87 */,
+/* 88 */,
+/* 89 */,
+/* 90 */,
+/* 91 */,
+/* 92 */,
+/* 93 */
+/***/ (function(module, exports) {
+
+// chart2
+// idnya : statistikPresensi
+$("#statistikPresensiPegawai").ready(function () {
+  if (!$("#statistikPresensiPegawai").length) return;
+  id = $("#statistikPresensiPegawai").attr('data');
+
+  function acakWarna(index) {
+    var kodeWarna = ['#23B7E7', '#FFB88C', '#007bff', '#6610f2', '#7266ba', '#f532e5', '#dc3545', '#fd7e14', '#fad732', '#37bc9b', '#20c997', '#17a2b8', '#6c757d', '#343a40', '#5d9cec', '#27c24c', '#23b7e5', '#ff902b', '#FEB58A', '#f05050', '#131e26', '#8bb8f1', '#2f80e7', '#43d967', '#1e983b', '#51c6ea', '#1797be', '#ffab5e', '#f77600', '#f47f7f', '#ec2121', '#58ceb1', '#2b957a', '#f763eb', '#e90bd6', '#9289ca', '#564aa3', '#243948', '#020304', '#fbe164', '#f3ca06', '#5d9cec', '#27c24c', '#23b7e5', '#ff902b', '#f05050', '#3a3f51'];
+    if (index > kodeWarna.length) index = index % kodeWarna.length;
+    return kodeWarna[index];
+  }
+
+  var doughnutData = {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: [],
+      pointStyle: 'dash'
+    }]
+  };
+  var doughnutOptions = {
+    legend: {
+      display: true,
+      position: 'right'
+    }
+  };
+  var doughnutctx = document.getElementById('statistikPresensiPegawai').getContext('2d');
+  var doughnutChart = new Chart(doughnutctx, {
+    data: doughnutData,
+    type: 'doughnut',
+    options: doughnutOptions
+  });
+
+  axios({
+    method: 'get',
+    url: 'api/data/statistik-presensi/' + id
+  }).then(function (response) {
+    iteration = 0;
+    $.each(response.data, function (index, value) {
+      doughnutData.labels.push(index);
+      doughnutData.datasets[0].data.push(value);
+      doughnutData.datasets[0].backgroundColor.push(acakWarna(iteration));
+      iteration += 1;
+    });
+    doughnutChart.update();
+  });
+});
 
 /***/ })
 /******/ ]);
