7063d81a5ba0e8898c27874fd8cd8f3fd604b49b
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-11-28 18:55:38 +0800

    fungsi kirim email raw

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\UserRepository;
+use App\Mail\ResetPassword;
+use Mail;
 
 class ResetPasswordController extends Controller
 {
@@ -14,6 +16,7 @@ class ResetPasswordController extends Controller
 
   public function submit(Request $request, UserRepository $user){
     $user = $user->where('username', $request->input)->orWhere('email', $request->input)->first();
-    dd($user);
+    if (!$user) dd('stop, you must validate !!!');
+    Mail::to($user->email)->send(new ResetPassword($user));
   }
 }
