1976bfc139d964f09b022f56efe1a09bc5f4a0a5
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-29 05:07:57 +0800

    mendefinisikan nama aplikasi

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
   |
   */
 
-  'name' => env('APP_NAME', 'Laravel'),
+  'name' => 'Aplikasi Presensi Dinas Pendidikan Kab. Banjar',
 
   /*
   |--------------------------------------------------------------------------
